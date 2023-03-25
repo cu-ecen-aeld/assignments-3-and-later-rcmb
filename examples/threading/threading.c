af005f26b1edd3d81803dca9ece5e1dc056d9669
Rcmb <rcmb@ubuntuvm>
2023-03-25 01:58:06 +0000

    Assignment 4 Part 1 complete

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -3,31 +3,76 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//Functions to add debug or error prints
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
 {
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
-}
+    //Cast the thread parameter to a pointer to the thread_data struct
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    //Wait before attempting to obtain the mutex
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    DEBUG_LOG("Wait over...obtaining mutex");
 
+    //Attempt to obtain the mutex
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
+      thread_func_args->thread_complete_success = false;
+      ERROR_LOG("Failed to obtain mutex");
+      return NULL;
+    }
+    
+    //Wait before releasing the mutex
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    DEBUG_LOG("Wait over...releasing mutex");
+    
+    //Release the mutex
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+      thread_func_args->thread_complete_success = false;
+      ERROR_LOG("Failed to release mutex");
+      return NULL;
+    }
+
+    thread_func_args->thread_complete_success = true;
+    
+    //Return the thread parameter
+    return thread_param;
+
+}
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
+     * Allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
      * using threadfunc() as entry point.
      *
      * return true if successful.
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    
+    // allocate memory for thread_data
+    struct thread_data* thread_data_ptr = malloc(sizeof(struct thread_data));
+    if (thread_data_ptr == NULL) {
+       ERROR_LOG("Failed to allocate memory for thread_data");
+       return false;
+    }
+
+    // load thread_data
+    thread_data_ptr->mutex = mutex;
+    thread_data_ptr->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data_ptr->wait_to_release_ms = wait_to_release_ms;
+    
+    // create thread and pass thread_data as argument
+    if (pthread_create(thread, NULL, threadfunc, (void*)thread_data_ptr) != 0) {
+       ERROR_LOG("Failed to create thread");
+       free(thread_data_ptr);
+       return false;
+    }
+
+    return true;
+
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -9,16 +9,17 @@
  */
 struct thread_data{
     /*
-     * TODO: add other values your thread will need to manage
-     * into this structure, use this structure to communicate
-     * between the start_thread_obtaining_mutex function and
-     * your thread implementation.
+     * Structure to communicate between the start_thread_obtaining_mutex function and
+     * the thread implementation.
      */
 
-    /**
-     * Set to true if the thread completed with success, false
-     * if an error occurred.
-     */
+    // The mutex that the thread needs to obtain and release.
+    pthread_mutex_t* mutex;
+    // The number of milliseconds that the thread should wait before obtaining the mutex.
+    int wait_to_obtain_ms;
+    // The number of milliseconds that the thread should wait before releasing the mut
+    int wait_to_release_ms;
+    // Set to true if the thread completed with success, false if an error occurred.
     bool thread_complete_success;
 };
 
